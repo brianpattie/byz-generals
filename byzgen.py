3484a8149256c8979b554033f2e9add693b495f5
Brian Pattie <bpattie@uvic.ca>
2018-11-22 14:51:15 -0800

    deleted the old version of expected()

--- byzgen.py
+++ byzgen.py
@@ -2,7 +2,7 @@ import sys
 import threading
 import queue
 
-# Nodes of the tree.  Implemented as recursive dictionaries.
+# Nodes of the OrderTree.  Implemented as recursive dictionaries.
 class OrderNode():
 
     def __init__(self):
@@ -48,21 +48,24 @@ class OrderNode():
 
         self.dict[order_path[0]].insert(order, order_path[1:])
 
-
+# A tree of OrderNodes.  Implemented using recursive dictionaries.
 class OrderTree():
 
     def __init__(self):
         self.root = OrderNode()
         self.num_nodes = 0
 
+    # Inserts a node.  Invokes the OrderNode's recursive insert() method.
     def insert(self, order, order_path):
         self.root.insert(order, order_path[1:])
         self.num_nodes += 1
 
+    # Evaluates the order tree to make a decision.  Invokes the OrderNode's recursive majority() method.
     def majority(self):
         self.root.majority()
         return self.root.value
 
+# Message objects are passed between generals on queues.
 class Message():
 
     def __init__(self, order, order_path, r_level):
@@ -76,7 +79,7 @@ class Message():
     def print(self):
         print(self.order + " " + str(self.order_path) + " " + str(self.r_level))
 
-
+# General threads.  Thread with id = 0 is the commander and all others are lieutenants.
 class General(threading.Thread):
 
     def __init__(self, id, loyal, queues, expected):
@@ -88,6 +91,7 @@ class General(threading.Thread):
         self.ordertree = OrderTree()
 
     def run(self):
+        # The commander (id = 0) doesn't receive any messages beyond the initial message from the main thread
         if self.id == 0:
             msg = self.queues[self.id].get()
             self.ordertree.insert(msg.order, msg.order_path)
@@ -97,7 +101,8 @@ class General(threading.Thread):
                 self.traitor_relay(msg)
 
             self.print_action(self.ordertree.majority())
-
+        # Lieutenants (id > 0) receive messages from other lieutenants until they have completed their
+        # recursive order tree.  Then they make a decision.
         else:
             while self.ordertree.num_nodes < self.expected:
                 msg = self.queues[self.id].get()
@@ -110,6 +115,7 @@ class General(threading.Thread):
 
             self.print_action(self.ordertree.majority())
 
+    # Relays the message correctly to the other generals
     def loyal_relay(self, msg):
 
         # Copy the message, add yourself to the order's history, decrement the recursion level
@@ -122,7 +128,8 @@ class General(threading.Thread):
             if i not in new_msg.order_path:
                 self.queues[i].put(new_msg.copy())
 
-
+    # Relays the message correctly to odd numbered generals and relays the
+    # opposite to even numbered generals
     def traitor_relay(self, msg):
         # Copy the message, add yourself to the order's history, decrement the recursion level
         loyal_msg = msg.copy()
@@ -155,16 +162,6 @@ class General(threading.Thread):
         else:
             print("Traitor General " + str(self.id) + " took action " + action)
 
-def expected(num_gen, r_level):
-
-    def r_expected(num_gen, r_level):
-        if r_level == 0:
-            return num_gen
-        else:
-            return num_gen * r_expected(num_gen - 1, r_level - 1)
-
-    return r_expected(num_gen, r_level) + 1
-
 # Math to determine how many messages each general expects to get total
 def expected(n, m):
     sum = 0
@@ -184,6 +181,7 @@ if len(sys.argv) < 4:
     print("Not enough arguments")
     exit()
 
+# Parse arguments
 gen_str = sys.argv[1]
 gen_num = len(sys.argv[1])
 order = sys.argv[2]
@@ -200,7 +198,7 @@ generals = []
 for i in range(0, len(gen_str)):
     generals.append(General(i, gen_str[i] == 'L', queues, expected))
 
-# Start threads
+# Start general threads
 for g in generals:
     g.start()
 
